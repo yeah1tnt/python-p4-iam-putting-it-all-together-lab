d2a6018c58e58e2d3e598b9408cde715595b2255
Tong Tang <pichip22@hotmail.com>
2023-09-24 20:36:08 -0400

    test

--- server/app.py
+++ server/app.py
@@ -8,19 +8,95 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+        img = data.get('image_url')
+        bio = data.get('bio')
+
+        if username and password:
+            user = User(username=username, image_url=img, bio=bio)
+            user_dict = user.to_dict()
+            user.password_hash = password
+
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+            return user_dict, 201
+        
+        return {'error':'422 Unprocessable Entity'}, 422
+
+            
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        # if 'user_id' in session:
+        if session.get('user_id'):
+            user = User.query.get(session['user_id'])
+            return user.to_dict(), 200
+        return {'error':'401 Unauthorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error':'401 Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204
+        return {'error':'401 Unauthorized'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.get(session['user_id'])
+            return [recipe.to_dict() for recipe in user.recipes], 200
+        return {'error':'401 Unauthorized'}, 401
+    
+    def post(self):
+        if session.get('user_id'):
+            data = request.get_json()
+
+            title = data.get('title')
+            instructions = data.get('instructions')
+            minutes_to_complete = data.get('minutes_to_complete')
+
+            try:
+                recipe = Recipe(
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete,
+                    user_id=session['user_id']
+                )
+                # recipe.user_id = session['user_id']
+
+                recipe_dict = recipe.to_dict()
+                recipe.user_id=session['user_id']
+
+                db.session.add(recipe)
+                db.session.commit()
+
+                return recipe_dict, 201
+            except IntegrityError:
+                return {'error':'422 Unprocessable Entity'}, 422
+        return {'error':'401 Unauthorized'}, 401
+
+                
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
